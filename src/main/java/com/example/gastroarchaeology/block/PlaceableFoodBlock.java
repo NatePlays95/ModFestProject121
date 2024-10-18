74982c274fd73ef1394416d1e0220b8907f9fe45
Natan Maia <natanmaiaplays@outlook.com>
2024-10-18 12:31:03 -0300

    placeable food is now abstract

--- src/main/java/com/example/gastroarchaeology/block/PlaceableFoodBlock.java
+++ src/main/java/com/example/gastroarchaeology/block/PlaceableFoodBlock.java
@@ -1,9 +1,132 @@
 package com.example.gastroarchaeology.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CandleCakeBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PlaceableFoodBlock extends Block {
+public abstract class PlaceableFoodBlock extends Block {
+
+    public static final IntegerProperty BITES = BlockStateProperties.BITES;
+
     public PlaceableFoodBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(BITES, Integer.valueOf(0)));
+    }
+
+
+
+
+    public abstract int getMaxBites();
+    public abstract boolean canAlwaysEat();
+
+    /**
+     * How many hunger bars a bite of this food fills. Max is 20.
+     */
+    public abstract int getBiteFullness();
+
+    public abstract float getBiteSaturation();
+
+    //Override this on children
+//    public VoxelShape getShapeByBite(BlockState state) {return Shapes.block();}
+    public abstract VoxelShape getShapeByBite(BlockState state);
+
+
+
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return this.getShapeByBite(state);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(BITES);
+    }
+
+    @Override
+    protected BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
+        return facing == Direction.DOWN && !state.canSurvive(level, currentPos)
+                ? Blocks.AIR.defaultBlockState()
+                : super.updateShape(state, facing, facingState, level, currentPos, facingPos);
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return level.getBlockState(pos.below()).isSolid(); //requires a block below
+    }
+
+
+
+
+    @Override
+    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos,
+                                              Player player, InteractionHand hand, BlockHitResult hitResult) {
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (level.isClientSide) {
+            if (this.eat(level, pos, state, player).consumesAction()) {
+                return InteractionResult.SUCCESS;
+            }
+
+            if (player.getItemInHand(InteractionHand.MAIN_HAND).isEmpty()) {
+                return InteractionResult.CONSUME;
+            }
+        }
+
+        return eat(level, pos, state, player);
+    }
+
+    protected InteractionResult eat(LevelAccessor level, BlockPos pos, BlockState state, Player player) {
+        if (!player.canEat(this.canAlwaysEat())) {
+            return InteractionResult.PASS;
+        } else {
+//            player.awardStat(Stats.EAT_CAKE_SLICE);
+            player.getFoodData().eat(this.getBiteFullness(), this.getBiteSaturation());
+            int i = state.getValue(BITES);
+            level.gameEvent(player, GameEvent.EAT, pos);
+            if (i < this.getMaxBites()) {
+                level.setBlock(pos, state.setValue(BITES, Integer.valueOf(i + 1)), 3);
+            } else {
+                level.removeBlock(pos, false);
+                level.gameEvent(player, GameEvent.BLOCK_DESTROY, pos);
+            }
+
+            return InteractionResult.SUCCESS;
+        }
+    }
+
+    @Override
+    protected boolean isPathfindable(BlockState state, PathComputationType pathComputationType) {
+        return false;
     }
 }
